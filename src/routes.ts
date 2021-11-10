7c286b2cb99b7a7bc1cbb8f583412d7dfce3742e
Victor Prim Büchele <victorprimbuchele@gmail.com>
2021-11-10 06:35:52 -0300

    Finalizado CRUD: criação de rotas update e delete

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,8 @@
 import { Router, Request, Response } from "express";
 import { appointmentService } from "./services/AppointmentService";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const routes = Router();
 
@@ -8,3 +11,7 @@ routes.get("/atendimentos/:id", appointmentService.listAService);
 routes.get("/atendimentos/", appointmentService.listAllServices);
 
 routes.post("/atendimentos", appointmentService.addAService);
+
+routes.patch("/atendimentos/:id", appointmentService.updateAService);
+
+routes.delete("/atendimentos/:id", appointmentService.deleteAService);
--- src/services/AppointmentService.ts
+++ src/services/AppointmentService.ts
@@ -42,7 +42,6 @@ class AppointmentService {
   async listAService(request: Request, response: Response) {
     try {
       const id: any = request.params.id;
-      console.log(id);
       const service = await prisma.atendimentos.findUnique({
         where: { id: parseInt(id) },
       });
@@ -76,11 +75,60 @@ class AppointmentService {
     }
   }
 
-  // async updateAService(request: Request, response: Response) {
-  //   try {
+  async updateAService(request: Request, response: Response) {
+    try {
+      const id: any = request.params.id;
+      const { cliente, pet, servico, status, observacoes, dataConsulta } =
+        request.body;
 
-  //   }
-  // }
+      const nome = clientValidation.validate(cliente);
+
+      const date = await datatimeValidation.validate(dataConsulta);
+
+      const service = await prisma.atendimentos.update({
+        where: { id: parseInt(id) },
+        data: {
+          cliente: nome,
+          pet: pet,
+          servico: servico,
+          status: status,
+          observacoes: observacoes,
+          dataConsulta: date,
+        },
+      });
+      return response.json({
+        status: 201,
+        message: "Busca realizada com sucesso",
+        data: service,
+      });
+    } catch (error) {
+      return response.json({
+        status: 400,
+        message: `Ocorreu um erro ${error}`,
+      });
+    }
+  }
+
+  async deleteAService(request: Request, response: Response) {
+    try {
+      const id: any = request.params.id;
+      console.log(id);
+
+      const service = await prisma.atendimentos.delete({
+        where: { id: Number(id) },
+      });
+
+      return response.json({
+        status: 201,
+        message: `O serviço do cliente ${service.cliente} excluído com sucesso`,
+      });
+    } catch (error) {
+      return response.json({
+        status: 400,
+        message: `Ocorreu um erro ${error}`,
+      });
+    }
+  }
 }
 
 export const appointmentService = new AppointmentService();
